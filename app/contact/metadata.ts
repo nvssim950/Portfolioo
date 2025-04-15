004e61a1df2bedc5b4d962ead979633fc303909e
NASSIM BAHEDDI <nassimbddm@gmail.com>
2025-04-15 13:11:53 +0100

    Delete app directory

--- app/contact/metadata.ts
+++
@@ -1,6 +0,0 @@
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Contact Us | trivix',
-  description: 'Get in touch with our team of software engineers. We specialize in Next.js, TypeScript, MongoDB, and PostgreSQL development.',
-}; 
--- app/metadata.ts
+++
@@ -1,9 +0,0 @@
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Les Batar | Web Development Experts',
-  description: 'Professional portfolio of Les Batar, a team of software engineers specializing in Next.js, TypeScript, MongoDB, and PostgreSQL development.',
-  icons: {
-    icon: '/favicon.ico',
-  }
-}; 
